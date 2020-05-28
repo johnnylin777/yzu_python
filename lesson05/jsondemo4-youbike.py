83e1d600fa96fc06a068d8afc10e0705acdfcb05
johnny <u8860035@gmail.com>
2020-05-28 20:41:53 +0800

    lesson05

--- lesson05/jsondemo4-youbike.py
+++ lesson05/jsondemo4-youbike.py
@@ -17,13 +17,13 @@ url = 'https://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc
 data = json.loads(requests.get(url).text)
 #print(data)
 youbikes = data.get('result').get('records')
-sbi = 10 # 可借數量
-bemp = 10 # 可還數量
+sbi = 1 # 可借數量
+bemp = 1 # 可還數量
 m = 1000
 
 for youbike in youbikes:
     lat = float(youbike.get('lat'))
     lng = float(youbike.get('lng'))
-    mm = haversine(121.297517, 25.056957, lng, lat)
+    mm = haversine(121.297760, 25.056595, lng, lat)    #樂技公司 121.297238, 25.019171 南崁25.056595, 121.297760
     if int(youbike.get('sbi')) >= sbi and int(youbike.get('bemp')) >= bemp and mm < m:
         print(youbike.get('sna'), youbike.get('sbi'), youbike.get('bemp'), mm , "m")
